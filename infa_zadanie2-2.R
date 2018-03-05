f5360e8a6af3235a4d59c628918be82c71281c8b
Agata <ag.kpp.37@gmail.com>
2018-03-05 22:48:56 +0100

    Update infa_zadanie2-2.R

--- infa_zadanie2-2.R
+++ infa_zadanie2-2.R
@@ -1,11 +1,13 @@
 print ("Za ile godzin wreszcie skoncza sie te studia?")
 today <- Sys.Date()
 koniec_tego_koszmaru <- as.Date("2021-06-30")
-print ("Jesli masz zamiar kiblowac wpisz inna date")
 
 hours <- as.numeric(difftime(koniec_tego_koszmaru, today), units="hours")
 dodac <- as.numeric(difftime(strptime("23:59", format = "%H:%M"), 
-                                  strptime("23:59", format = "%H:%M")), units="hours")
-hours <- hours + dodac
+                             strptime("23:59", format = "%H:%M")), units="hours")
+
+lata_kiblowania <- c(0*8760) #w miejsce 0 mozna wpisac lata jakie sie kiblowało
+
+hours <- hours + dodac + lata_kiblowania
 
 paste("Ten koszmar skonczy sie za...", hours, "godzin")
