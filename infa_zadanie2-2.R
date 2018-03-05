ee9a319e6a39d2e8cd9706fd96fb06bd875fd715
Agata <ag.kpp.37@gmail.com>
2018-03-05 21:31:29 +0100

    Update infa_zadanie2-2.R

--- infa_zadanie2-2.R
+++ infa_zadanie2-2.R
@@ -5,7 +5,7 @@ print ("Jesli masz zamiar kiblowac wpisz inna date")
 
 godziny <- as.numeric(difftime(koniec_tego_koszmaru, today), units="hours")
 dodac <- as.numeric(difftime(strptime("23:59", format = "%H:%M"), 
-                                  strptime("23:59", format = "%H:%M")), units="hours")
+                                  strptime("23:59", format = "%H:%M")), units="godziny")
 godziny <- godziny + dodac
 
 paste("Ten koszmar skonczy sie za...", hours, "godzin")
