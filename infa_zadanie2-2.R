1830e228f1767e36aa6b6f0239cda4b2d50a152d
Agata <ag.kpp.37@gmail.com>
2018-03-06 08:10:19 +0100

    Update infa_zadanie2-2.R

--- infa_zadanie2-2.R
+++ infa_zadanie2-2.R
@@ -5,9 +5,10 @@ koniec_tego_koszmaru <- as.Date("2021-06-30")
 hours <- as.numeric(difftime(koniec_tego_koszmaru, today), units="hours")
 dodac <- as.numeric(difftime(strptime("23:59", format = "%H:%M"), 
                              strptime("23:59", format = "%H:%M")), units="hours")
+print ("Czy kiblowałeś, jeśli tak to podaj ile to bylo, w latach?")
 
-lata_kiblowania <- c(0*8760) #w miejsce 0 mozna wpisac lata jakie sie kiblowało
+lata_kiblowania <- c(0) #w miejsce 0 mozna wpisac lata jakie sie kiblowało
 
-hours <- hours + dodac + lata_kiblowania
+hours <- hours + dodac + (lata_kiblowania * 8760)
 
 paste("Ten koszmar skonczy sie za...", hours, "godzin")
