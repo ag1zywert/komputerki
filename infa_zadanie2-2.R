bb506cfa43a617bf308cd9e0b120b081d8512a13
Agata <ag.kpp.37@gmail.com>
2018-03-06 20:28:26 +0100

    its done

--- infa_zadanie2-2.R
+++ infa_zadanie2-2.R
@@ -7,7 +7,7 @@ dodac <- as.numeric(difftime(strptime("23:59", format = "%H:%M"),
                              strptime("23:59", format = "%H:%M")), units="hours")
 print ("Czy kiblowałeś, jeśli tak to podaj ile to bylo, w latach?")
 
-lata_kiblowania <- c(0) #w miejsce 0 mozna wpisac lata jakie sie kiblowało
+lata_kiblowania <- c(0) #w miejsce 0 mozna wpisac lata jakie sie kiblowało czy coś tam 
 
 hours <- hours + dodac + (lata_kiblowania * 8760)
 
