ac46badf5c2d905a76ba4bf2c5debe35fb8d0879
Agata <ag.kpp.37@gmail.com>
2018-03-07 21:57:02 +0100

    zmiany

--- infa_zadanie2-5.R
+++ infa_zadanie2-5.R
@@ -2,10 +2,11 @@
 # To nie działa ale kierunek z grubsza dobry
 # Rozwiązanie z fibonacci_java dobre tylko przepisz 
 # w DOBRYM STYLU na R:)
+#JA: niestety nie mam dobrego stylu:(
 
 fibonacci <- function(n, a=1, b=1){ 
-  if (n == 1) return(a)
-  if (n == 2) return(b)
+  if(n == 1) return(a)
+  if(n == 2) return(b)
   return(fibonacci(n-1,b,a+b)) #nie wiem czy to coś zmienia
   
 } 
