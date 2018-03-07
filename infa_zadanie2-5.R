5e1de0cdf745196e31b53d42b90ced7ff192fa64
Agata <ag.kpp.37@gmail.com>
2018-03-07 21:36:43 +0100

    zmiany zmiany zmiany

--- infa_zadanie2-5.R
+++ infa_zadanie2-5.R
@@ -3,10 +3,9 @@
 # Rozwiązanie z fibonacci_java dobre tylko przepisz 
 # w DOBRYM STYLU na R:)
 
-fibonacci  <- function(n){
- 
+fibonacci <- function(n, a=1, b=1){ 
+  if (n == 1) return(a)
+  if (n == 2) return(b)
+  return(fibonacci(n-1,b,a+b)) #nie wiem czy to coś zmienia
   
-  if (length(n) > 1) return((n, fib))  
-if (n == 1) return(1) 
-if (n == 2) return(1) }
-return(fibonacci(n-1)+fibonacci(n-2)) 
+} 
