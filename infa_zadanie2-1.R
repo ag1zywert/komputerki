c95062874d5092d2949ad9609cee3a862a63734a
Agata <ag.kpp.37@gmail.com>
2018-03-07 18:04:09 +0100

    Update infa_zadanie2-1.R

--- infa_zadanie2-1.R
+++ infa_zadanie2-1.R
@@ -7,14 +7,14 @@
 
 kwartal_1 <- c ("Styczen„", "Luty", "Marzec")
 kwartal_2 <- c ("Kwiecen„", "Maj", "Czerwiec")
-kwartal_3 <- c ("Lipiec", "Sierpien", "Wrzesien")
+kwartal_3 <- c ("Lipiec", "Sierpien„", "Wrzesien„")
 kwartal_4 <- c ("Pazdziernik", "Listopad", "Grudzien„") 
 rok <- c (kwartal_1, kwartal_2, kwartal_3, kwartal_4)
 rok [1:2]
 rok [8:12]
-nchar (rok)
-mean (nchar(rok))
-sd (nchar(rok))
-mean (nchar(rok [12:3]))  
-sd (nchar(rok [12:3]))
+ile_liter <- nchar(rok) 
+mean (ile_liter)
+sd(ile_liter)
+mean(ile_liter[12:3])
+sd(ile_liter[12:3])
 subset (rok, endsWith(rok, "ec"))
