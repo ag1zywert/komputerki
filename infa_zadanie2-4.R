4c8adc36471df12ff065af229b830837b8e471b4
Agata <ag.kpp.37@gmail.com>
2018-03-09 15:35:04 +0100

    chyba działa teraz

--- infa_zadanie2-4.R
+++ infa_zadanie2-4.R
@@ -3,10 +3,10 @@
 #JA: Chyba się poddaję trochę
 
 lata <- 1800
-while (lata <= 2018) {  
- if (((lata %% 4) == 0) & ((lata %% 100) != 0)
- else if(lata %% 400 == 0) 
-      lata(i)
-      i -> i + 1
+while(lata <= 2018) {
+  if (((lata %% 4) == 0) & ((lata %% 100) != 0) || ((lata %% 400) == 0)) #nie wiem czy to się tak zapisuje
+    print(lata)
+  lata <-  lata + 1
 }
+
 
