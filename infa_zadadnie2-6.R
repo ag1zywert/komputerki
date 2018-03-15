8bd65a52f4d5485450671c44beec20052e53780b
Agata <ag.kpp.37@gmail.com>
2018-03-15 11:18:47 +0100

    Done

--- infa_zadadnie2-6.R
+++ infa_zadadnie2-6.R
@@ -4,7 +4,7 @@ choineczka <- function(a) {
   for (i in 1:a) {
     przerwa <- paste(przerwa, " ")
   }
-  print(paste(przerwa, igla = "*"))
+  print(paste(przerwa, igla = "*")) #tak zeby bylo tradycyjniej
 }
 for (i in 1:3) {
   for(i in 1:6)
