65c58c6a71ffa0a7cff1df498a76b84fdfd724cc
Agata <ag.kpp.37@gmail.com>
2018-03-20 17:40:55 +0100

    zadania 5
    Jakby co 5-8 dodam też

---
+++ zadania5.R
@@ -0,0 +1,39 @@
+library (tidyverse)
+str(movies)
+movies
+
+#zadanie 5-2
+
+filter(movies, year == 2005, Comedy == 1)
+
+# zadanie 5-3
+
+select(movies, title, year, budget) %>% arrange(movies, desc(budget))
+
+#zadanie 5-4
+
+filter(movies, Animation == 1, year >=1990 & year < 2000) %>%
+arrange(movies, desc(rating))
+
+#zadanie 5-5
+
+dramaty <- filter(movies, Drama == 1)
+arrange(dramaty, desc(length))
+
+#zadanie 5-6
+
+mpaa_rating <- group_by(movies, mpaa)
+summarise (mpaa_rating, srednia = mean(rating), odchylenie = mad(rating))
+
+#zadanie 5-7
+
+dobre_filmy <- filter(movies, votes > 100, budget > 3000000)
+  ggplot(data = dobre_filmy) +
+  geom_point(mapping = aes(x = budget, y = rating, alpha = 3/10)) +
+    geom_smooth(mapping = aes(x = budget, y = rating)) +
+    labs(title = "Czy wy¿szy bud¿et powoduje, ¿e film jest lepszy?")
+    
+
+
+
+  
